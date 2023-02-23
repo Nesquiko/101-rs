fbfd16d7f2280cc128492e9866d4a2abe237dc97
Nesquiko <lukas.castven@gmail.com>
2023-02-23 12:37:44 +0100

    exercises 1

--- exercises/A1/1-basic-syntax/src/bin/03.rs
+++ exercises/A1/1-basic-syntax/src/bin/03.rs
@@ -1,7 +1,15 @@
 fn main() {
     let input = [23, 82, 16, 45, 21, 94, 12, 34];
 
-    // TODO
+    let mut largest = input[0];
+    let mut minimum = input[0];
+    for num in input {
+        if num > largest {
+            largest = num;
+        } else if num < minimum {
+            minimum = num;
+        }
+    }
 
-    println!("{} is largest and {} is smallest");
+    println!("{} is largest and {} is smallest", largest, minimum);
 }
